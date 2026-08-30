1bee24822ef4ccab7e40b8b18ff54e5192c36cd7
Saurabh Tripathi <tripathisaurabh332@gmail.com>
2026-08-31 00:37:59 +0530

    set cor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
+})
+
+connectDB()
+.then(()=>{
+    const pr= process.env.PORT || 800;
+    app.listen(pr , ()=>{ 
+        console.log(`Server is running at PORT : ${pr}`);
+    })
+    app.on(`error`,(error)=>{
+        console.error("Error connecting to MongoDB:", error);
+        throw error
+    })
+})
+.catch((err)=>{
+    console.log('MONGODB is failed',err)
 })
